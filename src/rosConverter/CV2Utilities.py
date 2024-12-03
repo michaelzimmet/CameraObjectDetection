48b3e45171eeada7bbb291dce314d8785c7a03bd
Michael <michi@zimmet.de>
2024-12-03 20:05:55 +0100

    added logic to display a images sequence in a cv2 frame

--- src/rosConverter/CV2Utilities.py
+++ src/rosConverter/CV2Utilities.py
@@ -7,9 +7,9 @@ bridge = CvBridge()
 
 
 def cvt_message_to_cv2(message):
-    timestamp = message['timestamp']
     data = message['data']
-    return deserialize_image(data)
+    return message, deserialize_image(data)
+
 
 
 def show_image(image):
@@ -21,3 +21,21 @@ def show_image(image):
 def deserialize_image(message_data):
     img_msg = rclpy.serialization.deserialize_message(message_data, Image)
     return bridge.imgmsg_to_cv2(img_msg, desired_encoding='passthrough')
+
+def query_video_images(messages: list):
+    images = []
+    for i in messages:
+        images.append(cvt_message_to_cv2(i)[1])
+
+    return images
+
+
+def play_image_sequence(images, delay=.03):
+    window_name = 'Image Sequence'
+    cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
+
+    for img in images:
+        cv2.imshow(window_name, img)
+        if cv2.waitKey(int(delay * 1000)) & 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()
--- src/rosConverter/main.py
+++ src/rosConverter/main.py
@@ -6,7 +6,7 @@ import Config
 
 from pathlib import Path
 from DatabaseUtilities import SQLiteConnection, load_sql_template
-from CV2Utilities import show_image, cvt_message_to_cv2
+from CV2Utilities import query_video_images, play_image_sequence
 
 
 def main():
@@ -17,8 +17,11 @@ def main():
     params = {'topic_name': config['topics']['image_rect_color']}
     sql_query = load_sql_template(Config.SQL_SELECT_ROWS_BY_TOPICNAME, params)
     print(sql_query)
-    messages = con.execute_select(sql_query, fetch_num=1, params=params)
-    show_image(cvt_message_to_cv2(messages))
+    messages = con.execute_select(sql_query, params=params)
+    images = query_video_images(messages)
+    play_image_sequence(images)
+
+
 
 def read_config_file(file: Path):
     with file.open('r') as f:
